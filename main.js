b313092650d3e9bc4300b5a7466dbde3b7aeea6e
Cid Donham <cid@livefyre.com>
2014-09-11 12:53:45 -0700

    little refatoring.

--- main.js
+++ main.js
@@ -18,81 +18,82 @@
 
   Livefyre.require(['streamhub-wall#3','streamhub-sdk#2','stream#0.2.2', 'streamhub-input#0.3'], function (LiveMediaWall, SDK, Stream, Input) {
       //console.log(HTTP);
+      var collection, wall, uploadButton, higestRated, content;
 
-      var collection = new SDK.Collection({
+      collection = new SDK.Collection({
           "articleId": "ory-1409851747000",
           "siteId": 333682,
           "network": "client-solutions.fyre.co"
       });
 
-      var wall = new LiveMediaWall({
+      wall = new LiveMediaWall({
         el: document.getElementById("wall"),
         postButton: true
       });
 
-      var uploadButton = new (Input.UploadButton)({
+      uploadButton = new (Input.UploadButton)({
         el: document.getElementById('upload-button')
       });
+
+      //get the most upvoted item
+      higestRated = new LiveMediaWall({
+        el:document.getElementById('highest'),
+        columns:1
+      });
+
+      //get most liked content from bootstrap
+      // and turn it into a Stream.Readable
+      content = $.get( "https://client-solutions.bootstrap.fyre.co/api/v3.0/site/333682/article/b3J5LTE0MDk4NTE3NDcwMDA=/top/likes/", function (response) {
+      }).done(function (response) {
+        var likedResContent = response.data.content[mostLiked(response.data)].content;
+        content = new SDK.Content(likedResContent);
+        higestRated.write(content);
+      });
+
+
+      function mostLiked (thedata) {
+        var highest = {},
+          i = 1;
+        //set item 0
+        // since our likeBy count doesnt' decrement, we need to set it to 0 if there is no likedBy array
+        highest.count = (thedata.content[0].content.annotations.likedBy.length) ? thedata.content[0].content.annotations.likedBy.length : 0;
+        highest.item = 0;
+
+        //replace it if item i has a bigger number of likes
+        for (i; i < thedata.content.length; i += 1) {
+          if (thedata.content[i].content.annotations.likedBy.length > highest.count) {
+            highest.count = thedata.content[i].content.annotations.likedBy.length;
+            highest.item = i;
+          }
+        }
+        //return the highest item
+        return highest.item;
+      }
+
+      //only show images
+      function itemsWithImages() {
+        var customTransform = new Stream.Transform({});
+        customTransform._transform = function (content, done) {
+          if (content.attachments.length == 0) {
+            done();
+          } else {
+            //TODO: attachment might be in array item > 0
+            if (content.attachments[0].type === "photo" || content.attachments[0].type === "video") {
+              return done(null, content);
+            }
+            done();
+          }
+        };
+
+        return customTransform;
+      }
 
       uploadButton.render();
       uploadButton.pipe(collection);
 
+      collection.createUpdater().pipe(itemsWithImages()).pipe(wall);
+      collection.createArchive().pipe(itemsWithImages()).pipe(wall.more);
 
-        //get the most upvoted item
-        var higestRated = new LiveMediaWall({
-            el:document.getElementById('highest'),
-            columns:1
-        });
-
-          var mostLiked = function(thedata){
-            var highest = {};
-            //set item 0
-            // since our likeBy count doesnt' decrement, we need to set it to 0 if there is no likedBy array
-            highest.count = (thedata.content[0].content.annotations.likedBy.length) ? thedata.content[0].content.annotations.likedBy.length : 0;
-            highest.item = 0;
-            //replace it if item i has a bigger number of likes
-            for (var i = 1; i < thedata.content.length; i++) {
-              if (thedata.content[i].content.annotations.likedBy.length > highest.count){
-                highest.count = thedata.content[i].content.annotations.likedBy.length;
-                highest.item = i;
-              }
-            }
-            //return the highest item
-            return highest.item;
-          };
-
-          //get most liked content from bootstrap
-          // and turn it into a Stream.Readable
-          var content = {};
-
-          $.get( "https://client-solutions.bootstrap.fyre.co/api/v3.0/site/333682/article/b3J5LTE0MDk4NTE3NDcwMDA=/top/likes/", function (response) {
-          }).done(function (response) {
-            var likedResContent = response.data.content[mostLiked(response.data)].content;
-            content = new SDK.Content(likedResContent);
-            higestRated.write(content);
-          });
-
-        //only show images
-        function itemsWithImages() {
-          var customTransform = new Stream.Transform({});
-          customTransform._transform = function (content, done) {
-            if (content.attachments.length == 0) {
-              done();
-            } else {
-              //TODO: attachment might be in array item > 0
-              if (content.attachments[0].type === "photo" || content.attachments[0].type === "video") {
-                return done(null, content);
-              }
-              done();
-            }
-          };
-
-          return customTransform;
-        }
-
-        collection.createUpdater().pipe(itemsWithImages()).pipe(wall);
-        collection.createArchive().pipe(itemsWithImages()).pipe(wall.more);
-
-    });
+  });
 
 }(Livefyre));
